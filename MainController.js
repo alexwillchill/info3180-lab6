71bcd069bd5fdbe74962222e8460b0740f6f0d99
Alex Williams <alex.williams04@mymona.uwi.edu>
2017-03-26 17:43:43 +0000

    step3

--- MainController.js
+++ MainController.js
@@ -5,4 +5,7 @@ app.controller("myCtrl",function($scope){
     $scope.addItem=function(){
         $scope.products.push($scope.addMe);
     };
+    $scope.removeItem= function(x){
+        $scope.products.splice(x,1);
+    }
 });
